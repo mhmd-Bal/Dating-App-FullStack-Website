d1f0e38c84784f1862a863b7d591abb40397fffb
mhmd-bal <mohammad.balouza@gmail.com>
2023-03-18 16:15:06 +0200

    Added the color palette and the different font sizes

--- Dating-App-BackEnd/routes/api.php
+++ Dating-App-BackEnd/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,7 +22,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group([
 
-    'namespace' => 'App\Http\Controllers',
     'prefix' => 'auth'
 
 ], function ($router) {
